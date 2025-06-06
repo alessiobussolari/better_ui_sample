a566a42480a80b929f25d616715dab395c9adf09
alessiobussolari <alessio@cosmic.tech>
2025-06-06 14:49:54 +0200

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # BetterUI General Components
+  include BetterUi::General::Components::Icon::IconHelper
+  include BetterUi::General::Components::Heading::HeadingHelper
+  include BetterUi::General::Components::Field::FieldHelper
+  include BetterUi::General::Components::Panel::PanelHelper
+  include BetterUi::General::Components::Button::ButtonHelper
+  include BetterUi::General::Components::Link::LinkHelper
+  include BetterUi::General::Components::Text::TextHelper
+  
+  # BetterUI Input Components
+  include BetterUi::General::Components::Input::Text::TextHelper
+  include BetterUi::General::Components::Input::Checkbox::CheckboxHelper
 end
